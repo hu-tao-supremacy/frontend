33b6bc92281b47fdd00ef58dd63e426dc6699a62
Pattarapon Kittisrisawai <pattarapon.kitti@gmail.com>
2021-02-17 20:25:10 +0700

    build: Add ant-design

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,11 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from 'vue';
+import { Button } from 'ant-design-vue';
+// import 'ant-design-vue/dist/antd.css';
+import App from './App.vue';
+import router from './router';
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(router).mount('#app')
+app.use(Button);
+
+app.use(router).mount('#app');

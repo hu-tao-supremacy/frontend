61993552559a9904a18fe357253d324f846a744e
Pattarapon Kittisrisawai <pattarapon.kitti@gmail.com>
2021-02-19 23:31:15 +0700

    style: Change ant template

--- vue.config.js
+++ vue.config.js
@@ -5,9 +5,17 @@ module.exports = {
         lessOptions: {
           // If you are using less-loader@5 please spread the lessOptions to options directly
           modifyVars: {
-            'primary-color': '#FF855F',
-            'link-color': '#1DA57A',
-            'border-radius-base': '2px',
+            'blue-6': '#6182F9',
+            'orange-6': '#FF855F',
+            'yellow-6': '#FAAD14',
+            'red-6': '#F5222D',
+            'green-6': '#3EB489',
+            'red-6': '#F5222D',
+            'primary-color': '@orange-6',
+            'info-color': '@orange-6',
+            'processing-color': '@orange-6',
+            'error-color': '@red-5',
+            'warning-color': '@yellow-5',
           },
           javascriptEnabled: true,
         },

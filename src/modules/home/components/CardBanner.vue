e5e314d12af020a2dcc586af25820141613dbcbd
Pattarapon Kittisrisawai <pattarapon.kitti@gmail.com>
2021-02-20 15:17:22 +0700

    style: Change the theme of tailwind

--- src/modules/home/components/CardBanner.vue
+++ src/modules/home/components/CardBanner.vue
@@ -1,3 +1,24 @@
 <template>
-  <h1>Hi</h1>
+  <div class="event-container flex rounded-2xl shadow">
+    <div class="w-2/3" :style="img">1</div>
+    <div class="w-1/3">2</div>
+  </div>
 </template>
+
+<script>
+export default {
+  setup() {
+    const img = "";
+
+    return { img };
+  }
+};
+</script>
+
+<style scoped>
+.event-container {
+  height: 320px;
+  background-color: red;
+  box-shadow: 0px 2px 20px rgba(0, 0, 0, 0.08);
+}
+</style>

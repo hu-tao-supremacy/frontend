dd616fb4e9bc8038c4ebdc47c73d31e5a158d4fb
Pattarapon Kittisrisawai <pattarapon.kitti@gmail.com>
2021-02-22 16:53:25 +0700

    feat: Create sign up modal

--- src/modules/global/TestPage.vue
+++ src/modules/global/TestPage.vue
@@ -1,11 +1,34 @@
 <template>
-  <h1>Test Page</h1>
+  <base-button @click="toggleModal('signup')">Show Sign Up Modal</base-button>
+  <ModalSignUp v-if="showSignUpModal" @close-modal="toggleModal('signup')" />
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, ref } from "vue";
+import BaseButton from "@/commons/UI/BaseButton.vue";
+import ModalSignUp from "./modal-sign-up/ModalSignUp.vue";
 
 export default defineComponent({
-  name: "TestPage"
+  name: "TestPage",
+  components: {
+    ModalSignUp,
+    BaseButton
+  },
+  setup() {
+    const showSignUpModal = ref(false);
+
+    function toggleModal(modal: string) {
+      switch (modal) {
+        case "signup":
+          showSignUpModal.value = !showSignUpModal.value;
+          break;
+      }
+    }
+
+    return {
+      showSignUpModal,
+      toggleModal
+    };
+  }
 });
 </script>

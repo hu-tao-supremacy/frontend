2abb7181e073e31e92158fa615d970b5395fd486
Pattarapon Kittisrisawai <pattarapon.kitti@gmail.com>
2021-02-22 17:04:10 +0700

    feat: Add Log In modal

--- src/modules/global/TestPage.vue
+++ src/modules/global/TestPage.vue
@@ -1,33 +1,42 @@
 <template>
   <base-button @click="toggleModal('signup')">Show Sign Up Modal</base-button>
   <ModalSignUp v-if="showSignUpModal" @close-modal="toggleModal('signup')" />
+  <base-button @click="toggleModal('login')">Show Log In Modal</base-button>
+  <ModalLogIn v-if="showLogInModal" @close-modal="toggleModal('login')" />
 </template>
 
 <script lang="ts">
 import { defineComponent, ref } from "vue";
 import BaseButton from "@/commons/UI/BaseButton.vue";
 import ModalSignUp from "./modal-sign-up/ModalSignUp.vue";
+import ModalLogIn from "./modal-log-in/ModalLogIn.vue";
 
 export default defineComponent({
   name: "TestPage",
   components: {
     ModalSignUp,
-    BaseButton
+    BaseButton,
+    ModalLogIn
   },
   setup() {
     const showSignUpModal = ref(false);
+    const showLogInModal = ref(false);
 
     function toggleModal(modal: string) {
       switch (modal) {
         case "signup":
           showSignUpModal.value = !showSignUpModal.value;
           break;
+        case "login":
+          showLogInModal.value = !showLogInModal.value;
+          break;
       }
     }
 
     return {
       showSignUpModal,
-      toggleModal
+      toggleModal,
+      showLogInModal
     };
   }
 });

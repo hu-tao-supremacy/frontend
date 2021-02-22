97ea44c9a23c3b6c315a1a2b1a00d1c5fd7939e1
Pattarapon Kittisrisawai <pattarapon.kitti@gmail.com>
2021-02-22 15:48:22 +0700

    style: Change padding of modal to fit design

--- src/commons/UI/BaseModal.vue
+++ src/commons/UI/BaseModal.vue
@@ -2,7 +2,7 @@
   <div
     class="w-screen h-screen bg-gray-4 fixed inset-0 z-50 bg-opacity-50 flex justify-center items-start pt-18"
   >
-    <section class="bg-white relative p-8 rounded-2xl">
+    <section class="bg-white relative px-8 pt-8 pb-4 rounded-2xl">
       <base-transparent-button class="cross-btn absolute" @click="closeModal"
         ><base-icon width="24px" height="24px"><XIcon /></base-icon
       ></base-transparent-button>

a4e824c31e07690077f60d973ae154f0b6c0e769
new5558 <new17353@gmail.com>
2021-02-24 00:43:52 +0700

    feat: add storybook example + fix bugs

---
+++ src/modules/home/components/card-banner/CardBanner.stories.ts
@@ -0,0 +1,28 @@
+import CardBanner from "./CardBanner.vue";
+import "@/index.css";
+import { app, Story } from "@storybook/vue3";
+import BaseIcon from "@/commons/UI/BaseIcon.vue";
+
+app.component("base-icon", BaseIcon);
+
+export default {
+  title: "Home/CardBanner",
+  component: CardBanner
+};
+
+interface PropTypes {
+  eventTitle: string;
+}
+
+const Template: Story<PropTypes> = args => ({
+  components: { CardBanner },
+  setup() {
+    return { args };
+  },
+  template: '<CardBanner v-bind="args" />'
+});
+
+export const Primary = Template.bind({});
+Primary.args = {
+  eventTitle: "CU Collective Investment"
+};

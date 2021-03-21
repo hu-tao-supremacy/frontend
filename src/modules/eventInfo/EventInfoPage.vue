196f2c7c14495fa9714bb60f2a2603fe09489f8d
Seqlet <ratchanoneks@gmail.com>
2021-03-21 14:49:04 +0700

    fix: event info page new layout

--- src/modules/eventInfo/EventInfoPage.vue
+++ src/modules/eventInfo/EventInfoPage.vue
@@ -9,30 +9,34 @@
         :eventBanner="test.eventBanner"
       />
       <div class="text-4xl font-heading mt-7">Event Information</div>
-      <div class="flex w-full h-full mt-3 mb-4 overflow-auto">
+      <div class="event w-full mt-3 mb-4 grid gap-4">
         <div class="flex flex-col">
           <EventDetail
             class="mb-3"
-            :style="{ maxWidth: '628px' }"
+            :style="{ maxWidth: '629px' }"
             :eventDetail="test.eventDetail"
           />
           <div class="text-lg font-heading">Organized by</div>
           <EventOrganizer
             class="mt-1"
-            :style="{ maxWidth: '628px' }"
+            :style="{ maxWidth: '629px' }"
             :eventOrg="test.eventOrganizer"
           />
         </div>
-        <div class="flex flex-col ml-4">
-          <div class="text-lg font-heading">Event Schedule</div>
-          <div class="event-schedule-container rounded-lg flex flex-col justify-center items-center mt-1 py-1 bg-gray-2">
-            <EventSchedule
-              class="mb-1"
-              v-for="schedule in test.eventsSchedule"
-              :style="{ maxWidth: '283px' }"
-              :key="schedule.id"
-              :eventsSchedule="schedule"
-            />
+        <div class="flex flex-col h-0 min-h-full">
+          <div class="text-lg font-heading mb-1">Event Schedule</div>
+          <div class="overflow-y-auto">
+            <div
+              class="event-schedule-container rounded-lg flex flex-col justify-center items-center pt-1 bg-gray-2"
+            >
+              <EventSchedule
+                class="mb-1"
+                v-for="schedule in test.eventsSchedule"
+                :style="{ maxWidth: '283px' }"
+                :key="schedule.id"
+                :eventsSchedule="schedule"
+              />
+            </div>
           </div>
         </div>
       </div>
@@ -69,7 +73,10 @@ export default defineComponent({
 .container {
   max-width: 960px;
 }
-.event-schedule-container{
+.event {
+  grid-template-columns: auto auto;
+}
+.event-schedule-container {
   width: 299px;
 }
 </style>

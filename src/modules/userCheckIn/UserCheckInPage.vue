fdca7d8f562c8dac14270a56b9ad7862bab73a67
Seqlet <ratchanoneks@gmail.com>
2021-03-27 00:52:22 +0700

    initial

---
+++ src/modules/userCheckIn/UserCheckInPage.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="bg-gray-1 flex flex-col p-4 justify-center items-center w-full">
+    <div class="container">
+      <InfoBannerNoBg :eventBanner="test.eventBanner" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import InfoBannerNoBg from "@/modules/userCheckIn/info-banner-no-bg/InfoBannerNoBg.vue";
+import testData from "@/modules/eventInfo/use-event-info/testData";
+
+export default defineComponent({
+  name: "UserCheckInPage",
+  components: {
+    InfoBannerNoBg
+  },
+  setup() {
+    const test = testData;
+    return {
+      test
+    };
+  }
+});
+</script>
+
+<style scoped>
+.container {
+  max-width: 960px;
+}
+</style>
---
+++ src/modules/userCheckIn/ticket-qr/TicketQR.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="flex flex-col bg-white rounded-2xl shadow-sm">
+    <div class="font-heading text-3xl">Ticket ID: {{}}</div>
+    <qrcode-vue :value="value" :size="size" level="H" />
+    <div></div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import BaseButton from "@/commons/UI/BaseButton.vue";
+import QrcodeVue from "qrcode.vue";
+
+export default defineComponent({
+  name: "TicketQR",
+  components: {
+    QrcodeVue,
+    BaseButton
+  },
+  props: {
+    ticket: {
+      type: Object as () => {},
+      required: true
+    }
+  }
+});
+</script>
+
+<style></style>

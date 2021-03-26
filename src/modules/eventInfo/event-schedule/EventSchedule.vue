0f04518bd51c49f90c169a51abfc2a6300fef2d8
Pattarapon Kittisrisawai <pattarapon.kitti@gmail.com>
2021-03-26 22:58:16 +0700

    chore: Prettier

--- src/modules/eventInfo/event-schedule/EventSchedule.vue
+++ src/modules/eventInfo/event-schedule/EventSchedule.vue
@@ -3,21 +3,21 @@
     <div
       class="w-8 bg-primary-6 text-white text-center flex flex-col justify-center flex-shrink-0"
     >
-        <div class="text-2xl">{{ eventsSchedule.day }}</div>
-        <div class="text-lg">{{ eventsSchedule.month }}</div>
+      <div class="text-2xl">{{ eventsSchedule.day }}</div>
+      <div class="text-lg">{{ eventsSchedule.month }}</div>
     </div>
-    <div
-      class="w-full bg-white flex flex-col justify-center pl-1"
-    >
-        <div class="text-xl font-heading text-primary-6">{{ eventsSchedule.time }}</div>
-        <div class="text-sm mt-0.5">{{ eventsSchedule.faculty }}</div>
+    <div class="w-full bg-white flex flex-col justify-center pl-1">
+      <div class="text-xl font-heading text-primary-6">
+        {{ eventsSchedule.time }}
+      </div>
+      <div class="text-sm mt-0.5">{{ eventsSchedule.faculty }}</div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "@vue/runtime-core";
-import { Schedule } from "@/commons/Interfaces/index"
+import { Schedule } from "@/commons/Interfaces/index";
 
 export default defineComponent({
   name: "EventSchdule",
@@ -29,5 +29,4 @@ export default defineComponent({
 });
 </script>
 
-<style>
-</style>
+<style></style>

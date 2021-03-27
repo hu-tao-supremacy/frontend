919bee3ab2d842a249aec78607c023100682268e
Seqlet <ratchanoneks@gmail.com>
2021-03-27 17:01:32 +0700

    feat: user check in page

--- src/modules/userCheckIn/UserCheckInPage.vue
+++ src/modules/userCheckIn/UserCheckInPage.vue
@@ -2,19 +2,22 @@
   <div class="bg-gray-1 flex flex-col p-4 justify-center items-center w-full">
     <div class="container">
       <InfoBannerNoBg :eventBanner="test.eventBanner" />
+      <TicketQR class='mt-3' :ticket="test.ticket"/>
     </div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import InfoBannerNoBg from "@/modules/userCheckIn/info-banner-no-bg/InfoBannerNoBg.vue";
-import testData from "@/modules/eventInfo/use-event-info/testData";
+import InfoBannerNoBg from "@/modules/userCheckIn/info-banner-transparent/InfoBannerTransparent.vue";
+import TicketQR from "@/modules/userCheckIn/ticket-qr/TicketQR.vue"
+import testData from "@/modules/userCheckIn/use-user-checkin/testData";
 
 export default defineComponent({
   name: "UserCheckInPage",
   components: {
-    InfoBannerNoBg
+    InfoBannerNoBg,
+    TicketQR
   },
   setup() {
     const test = testData;

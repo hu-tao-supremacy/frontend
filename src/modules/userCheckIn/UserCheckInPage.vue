d9c75696396c224ef6b5c437ac1bfaca88fd8f4e
Seqlet <ratchanoneks@gmail.com>
2021-03-27 17:13:07 +0700

    feat: components story books

--- src/modules/userCheckIn/UserCheckInPage.vue
+++ src/modules/userCheckIn/UserCheckInPage.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="bg-gray-1 flex flex-col p-4 justify-center items-center w-full">
     <div class="container">
-      <InfoBannerNoBg :eventBanner="test.eventBanner" />
+      <InfoBannerTransparent :eventBanner="test.eventBanner" />
       <TicketQR class='mt-3' :ticket="test.ticket"/>
     </div>
   </div>
@@ -9,14 +9,14 @@
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import InfoBannerNoBg from "@/modules/userCheckIn/info-banner-transparent/InfoBannerTransparent.vue";
+import InfoBannerTransparent from "@/modules/userCheckIn/info-banner-transparent/InfoBannerTransparent.vue";
 import TicketQR from "@/modules/userCheckIn/ticket-qr/TicketQR.vue"
 import testData from "@/modules/userCheckIn/use-user-checkin/testData";
 
 export default defineComponent({
   name: "UserCheckInPage",
   components: {
-    InfoBannerNoBg,
+    InfoBannerTransparent,
     TicketQR
   },
   setup() {
--- src/modules/userCheckIn/use-user-checkin/testData.ts
+++ src/modules/userCheckIn/use-user-checkin/testData.ts
@@ -1,7 +1,5 @@
 const testData = {
   eventBanner: {
-    bannerImg: "https://picsum.photos/1000",
-    bannerImgHash: "LEHV6nWB2yk8pyo0adR*.7kCMdnj",
     profileImg: "https://picsum.photos/500",
     profileImgHash: "LEHV6nWB2yk8pyo0adR*.7kCMdnj",
     title: "Information and Communication Event (ICE)",

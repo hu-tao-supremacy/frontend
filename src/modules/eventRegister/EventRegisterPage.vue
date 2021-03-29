a8f9f954628caa1ef82bd087c4ef85a72b39dc6d
Seqlet <ratchanoneks@gmail.com>
2021-03-29 16:34:56 +0700

    feature: initial status bar

---
+++ src/modules/eventRegister/EventRegisterPage.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="bg-gray-1 flex flex-col p-4 justify-center items-center w-full">
+    <div class="container">
+      <RegistrationStatus/>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import testData from "@/modules/userCheckIn/use-user-checkin/testData";
+import RegistrationStatus from "@/commons/UI/RegistrationStatus.vue"
+
+export default defineComponent({
+  name: "EventRegisterPage",
+  components: {
+    RegistrationStatus
+  },
+  setup() {
+    const test = testData;
+    return {
+      test
+    };
+  }
+});
+</script>
+
+<style scoped>
+.container {
+  max-width: 960px;
+}
+</style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -4,6 +4,7 @@ import Home from "../modules/home/Home.vue";
 const Test = () => import("@/modules/global/TestPage.vue");
 const EventInfo = () => import("@/modules/eventInfo/EventInfoPage.vue");
 const UserCheckIn = () => import("@/modules/userCheckIn/UserCheckInPage.vue");
+const EventRegister = () => import("@/modules/eventRegister/EventRegisterPage.vue");
 
 const router = createRouter({
   history: createWebHistory(),
@@ -23,7 +24,11 @@ const router = createRouter({
     {
       path: "/user-checkin",
       component: UserCheckIn
-    }
+    },
+    {
+      path: "/event-register",
+      component: EventRegister
+    },
   ]
 });
 

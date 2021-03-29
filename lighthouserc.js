7b38165f4d4058dedb22eeba53a907bd990abf62
new5558 <new17353@gmail.com>
2021-03-29 21:06:13 +0700

    feat: upload to server

--- lighthouserc.js
+++ lighthouserc.js
@@ -9,7 +9,9 @@ module.exports = {
             url: ["https://127.0.0.1:8000/", "https://127.0.0.1:8000/test/", "https://127.0.0.1:8000/event/", "https://127.0.0.1:8000/user-checkin/"]
         },
         upload: {
-            target: "temporary-public-storage"
+            target: 'lhci',
+            serverBaseUrl: 'https://lit-ocean-90649.herokuapp.com/',
+            token: process.env.LHCI_SERVER_TOKEN,
         }
     }
 };

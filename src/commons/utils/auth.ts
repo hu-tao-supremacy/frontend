1e0b8048bb4729661f948ab5376348cc45b043aa
new5558 <new17353@gmail.com>
2021-04-14 23:15:42 +0700

    fix: redirect error with param path

--- src/commons/utils/auth.ts
+++ src/commons/utils/auth.ts
@@ -1,5 +1,5 @@
 export const login = () => {
-  const redirectPage = `${window.location.origin}/login?target=${window.location}`;
+  const redirectPage = `${window.location.origin}/login?target=${window.location.pathname}`;
   window.location.replace(
     `https://account.it.chula.ac.th/html/login.html?service=${redirectPage}`
   );

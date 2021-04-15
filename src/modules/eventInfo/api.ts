fba6131bb20f80d1f0c7947dc30eba5933a96358
new5558 <new17353@gmail.com>
2021-04-16 04:12:25 +0700

    fix: organization

--- src/modules/eventInfo/api.ts
+++ src/modules/eventInfo/api.ts
@@ -19,6 +19,7 @@ export const useEventById = (variables: GetEventByIdQueryVariables) =>
             name
           }
           organization {
+            id
             name
             abbreviation
             description

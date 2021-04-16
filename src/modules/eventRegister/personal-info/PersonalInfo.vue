63ce2314e1bd7750fcff45d442e795fbbdb120eb
Pattarapon Kittisrisawai <pattarapon.kitti@gmail.com>
2021-04-16 10:51:45 +0700

    feat: Hard code personal info

--- src/modules/eventRegister/personal-info/PersonalInfo.vue
+++ src/modules/eventRegister/personal-info/PersonalInfo.vue
@@ -20,7 +20,7 @@
       <div class="flex flex-col w-1/3">
         <div>Gender</div>
         <BaseTextInput
-          :value="user.gender"
+          :value="'M'"
           class="w-full h-4 disabled: bg-white text-gray-5"
           disabled
         />
@@ -38,7 +38,7 @@
       <div class="flex flex-col w-1/3 mr-4">
         <div>Faculty</div>
         <BaseTextInput
-          :value="user.chulaId"
+          :value="'Engineering'"
           class="w-full h-4 disabled: bg-white text-gray-5"
           disabled
         />
